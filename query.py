e7aacdfe0157140d4b688603bf1988b18778a3ce
Chang Jian Rong <cjr030918d@gmail.com>
2023-05-01 17:07:35 +0800

    Add functions to call database stored procedures

--- query.py
+++ query.py
@@ -28,3 +28,34 @@ class DatabaseConnection:
         with self.connection.cursor() as cursor:          
             cursor.execute(function.value, params)
             return cursor.fetchone()[0]
+        
+
+    def create_credentials(self, user_id, site, username, password, pin=None):
+        args = (user_id, site, username, password, pin)
+
+        with self.connection.cursor() as cursor:
+            cursor.callproc("create_credentials", args)
+            return cursor.fetchone()
+    
+
+    def create_user(self, name, email, root_password):
+        args = (name, email, root_password)
+
+        with self.connection.cursor() as cursor:
+            cursor.callproc("create_user", args)
+            return cursor.fetchone()
+
+
+    def update_credential(self, credential_id, site=None, username=None, password=None, pin=None):
+        args = (credential_id, site, username, password, pin)
+
+        with self.connection.cursor() as cursor:
+            cursor.callproc("get_credentials", args)
+            return cursor.fetchone()
+
+    def update_user(self, name=None, email=None, root_password=None):
+        args = (name, email, root_password)
+
+        with self.connection.cursor() as cursor:
+            cursor.callproc("update", args)
+            cursor.fetchone()
