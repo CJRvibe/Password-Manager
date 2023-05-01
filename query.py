1ca2f2f579dc33355152256be61f25e66d3576b5
Chang Jian Rong <cjr030918d@gmail.com>
2023-05-01 16:38:01 +0800

    Create function to call sql functions

--- query.py
+++ query.py
@@ -1,10 +1,21 @@
+from enum import Enum
+from typing import Sequence
 import mysql.connector
+from argon2 import PasswordHasher
 
+class SQLFunctions(Enum):
+     GET_PASSWORD_HASH = "SELECT get_password_hash(%s)"
+     GET_USER_ID = "SELECT get_user_id(%s)"
+     GET_CREDENTIAL_ID = "SELECT get_credential_id(%s)"
+
+     
 
 class DatabaseConnection:
 
     def __init__(self, user, password, database) -> None:
         self.database_connect(user, password, database)
+        self.logged_in_id = None
+        self.__ph = PasswordHasher()
 
     
     def database_connect(self, user, password, database):
@@ -14,3 +25,9 @@ class DatabaseConnection:
             database=database)
         
         self.connection = connection
+
+    
+    def call_SQL_function(self, function: Enum, params: Sequence):
+        with self.connection.cursor() as cursor:          
+            cursor.execute(function.value, params)
+            return cursor.fetchone()[0]
