c068c407e010a46a42c9011f7a7527a2a4a69f20
Chang Jian Rong <cjr030918d@gmail.com>
2023-05-01 19:35:54 +0800

    Create simple class to act as the interface of the application
    Create PasswordManagerInterface with an __init__ method and nothing else.

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,2 +1,10 @@
+from query import SQLFunctions, DatabaseConnection
+from argon2 import PasswordHasher
 
 
+class PasswordManagerInterface:
+
+    def __init__(self, db_user, db_password, db_name):
+        self.__db = DatabaseConnection(db_user, db_password, db_name)
+        self.__ph = PasswordHasher()
+        self.user = None
