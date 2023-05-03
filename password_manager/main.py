4a02d0244a2728ad21012d1403a730009be5d8a0
Chang Jian Rong <cjr030918d@gmail.com>
2023-05-03 14:35:46 +0800

    Add login function to PasswordManagerInterface
    The function logs the user in and saves its user_id

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,5 +1,5 @@
-from query import SQLFunctions, DatabaseConnection
-from argon2 import PasswordHasher
+from .query import SQLProcedures, DatabaseConnection
+from argon2 import PasswordHasher, exceptions
 
 
 class PasswordManagerInterface:
@@ -7,4 +7,15 @@ class PasswordManagerInterface:
     def __init__(self, db_user, db_password, db_name):
         self.__db = DatabaseConnection(db_user, db_password, db_name)
         self.__ph = PasswordHasher()
-        self.user = None
+        self.__user_id = None
+
+    
+    def login(self, username, password):
+        user = self.__db.call_SQL_procedure(SQLProcedures.GET_USER, (username, ))
+        hash = user[3]
+        try:
+            self.__ph.verify(hash, password)
+            print(f"logged in as {user[1]}")
+            self.__user_id = user[0] 
+        except exceptions.VerifyMismatchError:
+            print("Wrong password, try again!")
