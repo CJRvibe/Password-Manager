9acede9819d1e4a7c0c0ff74be209fd40ea1bea0
Chang Jian Rong <cjr030918d@gmail.com>
2023-05-03 16:52:23 +0800

    Add function for getting credentials for a logged in user

--- password_manager/main.py
+++ password_manager/main.py
@@ -9,6 +9,10 @@ class PasswordManagerInterface:
         self.__ph = PasswordHasher()
         self.__user_id = None
 
+
+    def __logged_in(self):
+        assert self.__user_id is not None
+
     
     def login(self, username, password):
         user = self.__db.call_SQL_procedure(SQLProcedures.GET_USER, (username, ))
@@ -19,3 +23,10 @@ class PasswordManagerInterface:
             self.__user_id = user[0] 
         except exceptions.VerifyMismatchError:
             print("Wrong password, try again!")
+
+    
+    def get_credentials(self):
+        try: self.__logged_in()
+        except AssertionError: print("You need to login first before calling this function")
+        credentials = self.__db.call_SQL_procedure(SQLProcedures.GET_CREDENTIALS, (self.__user_id, ))
+        return credentials
