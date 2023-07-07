99bea6590c851d51b0f808606fdb2dfa80e52ce9
Chang Jian Rong <cjr030918d@gmail.com>
2023-07-07 22:33:19 +0800

    Add functions to create account and add credentials
    Add new methods create_credentials and create_account. Also edit the interface to store the password hash so that algorithm encryption is the same for all credentials.

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,5 +1,11 @@
-from .query import SQLProcedures, DatabaseConnection
+import base64
+import os
+from pathlib import Path
 from argon2 import PasswordHasher, exceptions
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from .query import SQLProcedures, DatabaseConnection
 
 
 class PasswordManagerInterface:
@@ -8,6 +14,8 @@ class PasswordManagerInterface:
         self.__db = DatabaseConnection(db_user, db_password, db_name)
         self.__ph = PasswordHasher()
         self.__user_id = None
+        self.__username = None
+        self.__phash = None
 
 
     def __logged_in(self):
@@ -20,7 +28,6 @@ class PasswordManagerInterface:
         try:
             self.__ph.verify(hash, password)
             print(f"logged in as {user[1]}")
-            self.__user_id = user[0] 
         except exceptions.VerifyMismatchError:
             print("Wrong password, try again!")
 
@@ -28,10 +35,44 @@ class PasswordManagerInterface:
             self.__db.call_SQL_procedure(SQLProcedures.UPDATE_USER, 
                                          (self.__user_id, None, None, self.__ph.hash(password)))
             
+        self.__user_id = user[0] 
+        self.__username = user[1]
+        self.__phash = user[3]
         *data, hashed_password = user
         return tuple(data)
-
     
+
+    def create_account(self, username, email, password):
+        password = self.__ph.hash(password)
+
+        data = (username, email, password)
+        new = self.__db.call_SQL_procedure(SQLProcedures.CREATE_USER, data)
+
+        salt = os.urandom(16)
+        secrets_path = Path(f"{username}_secrets.bin")
+        secrets_path.write_bytes(salt)
+        print(f"New account created with username {username}")
+    
+
+    def create_credentials(self, root_password: str, site, username, password: str):
+        self.__logged_in()
+        self.__ph.verify(self.__phash, root_password)
+
+        salt_path = Path(f"{self.__username}_secrets.bin")
+        salt = salt_path.read_bytes()
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=480000,
+            )
+        key = base64.urlsafe_b64encode(kdf.derive(root_password.encode("utf-8")))
+        f = Fernet(key)
+        encrypted_password = f.encrypt(password.encode("utf-8"))
+
+        data = (self.__user_id, site, username, encrypted_password)
+        credential = self.__db.call_SQL_procedure(SQLProcedures.CREATE_CREDENTIALS, data)
+
     def get_credentials(self):
         try: self.__logged_in()
         except AssertionError: print("You need to login first before calling this function")
